79df6a04d21480c4e1fff794acb90accece0b5d8
kukcat <galaxycraft3103@gmail.com>
2022-01-19 01:42:46 +0200

    added BoardItem

--- script.js
+++ script.js
@@ -2,12 +2,20 @@
 //==================== vars and events 
 
 let addButton = document.querySelector(".add_btn")
-let cardPlacement = document.querySelector(".card_wrapper")
+let boardPlacement = document.querySelector(".board_wrapper")
 let resetBtn = document.querySelector(".reset_btn");
 
 let boardArr = []
 let boardItemArr = []
-let colorArr = ['#ff0000', '#00ff00', '#0000ff', '#cccccc']
+let currentColorId = 3;
+
+let colorArr = new Map([
+    [0, '#ff0000'],
+    [1, '#00ff00'],
+    [2, '#0000ff'],
+    [3, '#ffff00']
+])
+
 let BoardId = 0;
 let itemId = 0;
 
@@ -33,7 +41,7 @@ let createBoard = (id,name,color) =>{
     boardArr.push(board);
     addToLocalStorage();
     BoardId++;
-
+    
     if (document.querySelector('.modal_window') != null){
         exitModal();
     }
@@ -42,21 +50,47 @@ let createBoard = (id,name,color) =>{
 
 let createBoardItem = (boardId) =>{
 
+    if(boardArr[boardId].cardCounter >= 10){
+        addError('В одной доске должно быть не более 10 задач!')
+        return 
+    }
+    
     let itemInfo = document.forms.createBoardItem.elements;
     let itemTag = [];
-    for(let i = 0; i<colorArr.length; i++){
+
+    if (itemInfo.title.value.length == 0 || itemInfo.title.value.length > 20) {
+        addError('Название задачи должно содержать от 1 до 20 символов!')
+        return 
+    }
+
+    if (itemInfo.text.value.length == 0 || itemInfo.text.value.length > 20000) {
+        addError('Задача должна содержать от 1 до 20000 символов!')
+        return 
+    }
+
+    for(let i = 0; i < colorArr.size; i++){
         if(itemInfo.tag[i].checked){
-            itemTag.push(itemInfo.tag[i].value)
+            itemTag.push(itemInfo.tag[i].value);
         }
     }
 
-    console.log(itemTag)
+    if (itemTag.length == 0 || itemTag.length > 2) {
+        addError('У задачи должно быть от 1 до 2 тегов!')
+        return 
+    }
 
+    boardItemArr.forEach(element => {
+        if (element.title == itemInfo.title) {
+            addError('Задачи с таким заголовком уже существует!')
+            return 
+        }
+    });
+    
     const boardItem = {
         'boardId':  boardId,
         'position': boardArr[boardId].cardCounter,
         'itemId': itemId,
-        'titile': itemInfo.title.value,
+        'title': itemInfo.title.value,
         'text': itemInfo.text.value,
         'expiredDate': itemInfo.expiredDate.value,
         'tag': itemTag
@@ -64,10 +98,14 @@ let createBoardItem = (boardId) =>{
 
     addBoardItem(boardItem);
     boardItemArr.push(boardItem);
-    addToLocalStorage();
     boardArr[boardId].cardCounter++
+    addToLocalStorage();
     itemId++
-
+    
+    if (document.querySelector('.modal_window') != null){
+        exitModal();
+    }
+    
 }
 
 
@@ -75,31 +113,32 @@ let createBoardItem = (boardId) =>{
 //=============================== drawing
 let addBoard = (element) =>{
 
-    let newCardItem = document.createElement('li');
-    newCardItem.classList.add('card');
-    newCardItem.setAttribute('board-index-number', element.id);
+    let newboardItem = document.createElement('li');
+    newboardItem.classList.add('board');
+    newboardItem.setAttribute('board-index-number', element.id);
 
-    cardPlacement.append(newCardItem);
-    newCardItem.style.backgroundColor = element.color + "B3";
-    newCardItem.innerHTML=`
-        <div class="card_header">
-            <input type="text" class="card_header_name" value="${element.name}">
-            <div class="card_settings">...</div>
+    boardPlacement.append(newboardItem);
+    newboardItem.style.backgroundColor = element.color + "B3";
+    newboardItem.innerHTML=`
+        <div class="board_header">
+            <input type="text" class="board_header_name" value="${element.name}">
+            <div class="board_settings">...</div>
         </div>
-        <div class="card_body">
-            <ul class="card_body_list">
+        <div class="board_body">
+            <ul class="board_body_list">
                 
             </ul>
         </div>
-        <div class="card_footer">
+        <div class="board_footer">
             <p class="add_board_item">Добавить карточку</p>
         </div>
     `
-    newCardItem.childNodes[1].childNodes[1].addEventListener('change', ()=>changeBoardName(newCardItem.childNodes[1].childNodes[1]))
-    newCardItem.childNodes[1].childNodes[3].addEventListener('click', () => addModalBoardSettings(element.id))
-    newCardItem.childNodes[5].childNodes[1].addEventListener('click', () => addBoardItemModal(element.id))
+    newboardItem.childNodes[1].childNodes[1].addEventListener('change',()=> changeBoardName(newboardItem.childNodes[1].childNodes[1]))
+    newboardItem.childNodes[1].childNodes[3].addEventListener('click', () => addModalBoardSettings(element.id))
+    newboardItem.childNodes[5].childNodes[1].addEventListener('click', () => addBoardItemModal(element.id))
 
 }
+
 
 let addBoardItemModal = (boardId) =>{
     let modalWindow = document.createElement('div');
@@ -119,13 +158,17 @@ let addBoardItemModal = (boardId) =>{
     <label for="">Тег: </label>
     <div class="tag_wrapper">`
 
-    for (let i = 0; i < colorArr.length; i++) {
+
+    let i = 0;
+    for (let entries of colorArr.entries()) {
+        
         modalValue += `
             <div class="tag_item">
-                <label class="tag_label" for="tag${i}" style="background-color: ${colorArr[i]};"></label>
-                <input type="checkbox" name="tag" id="tag${i}" class="tag_input" value="${colorArr[i]}">
+                <label class="tag_label" for="tag${i}" style="background-color: ${entries[1]};"></label>
+                <input type="checkbox" name="tag" id="tag${i}" class="tag_input" value="${entries[0]}">
             </div>
         `
+        i++
     }
 
     
@@ -142,9 +185,9 @@ let addBoardItemModal = (boardId) =>{
         </form>`
     
     modalWindow.innerHTML = modalValue;
-    console.log()
+    
     modalWindow.childNodes[1].childNodes[1].childNodes[19].childNodes[1].addEventListener('click', () => createBoardItem(boardId));
-
+    
     modalWindow.childNodes[1].childNodes[1].childNodes[19].childNodes[3].addEventListener('click', () => exitModal())
 
 }
@@ -171,6 +214,25 @@ let addModalBoardSettings = (boardId) =>{
     modalWindow.childNodes[1].childNodes[7].childNodes[1].addEventListener('click', () => changeBoardColor(boardId, modalWindow.childNodes[1].childNodes[5].value))
     modalWindow.childNodes[1].childNodes[7].childNodes[3].addEventListener('click', () => exitModal())
     modalWindow.childNodes[1].childNodes[9].addEventListener('click', () => deleteBoard(boardId))
+}
+
+let changeBoardItemModal = (boardItemId) =>{
+    let modalWindow = document.createElement('div');
+    modalWindow.classList.add('modal_window');
+    document.querySelector('body').append(modalWindow);
+    modalWindow.innerHTML = `
+        <div class="window">
+            <input type="text" value="${boardItemArr[boardItemId].title}" class="">
+            <textarea>${boardItemArr[boardItemId].text}</textarea>
+
+            <div class="btn_wrapper">
+                <div class="add_item_btn">Изменить цвет</div>
+                <div class="cancel_item_btn">Выйти</div>
+            </div>
+            <div class="delete_item_btn">Удалить доску</div>
+
+        </div>
+    `
 }
 
 let addModalBoard = () =>{
@@ -201,7 +263,7 @@ let addModalBoard = () =>{
 //=======================================
 let changeBoardName = (element) =>{
     
-    let idBoard = element.closest('.card').getAttribute('board-index-number');
+    let idBoard = element.closest('.board').getAttribute('board-index-number');
     boardArr[idBoard].name = element.value;
     addToLocalStorage();
 
@@ -210,14 +272,33 @@ let changeBoardName = (element) =>{
 let changeBoardColor = (boardId, color) =>{
     boardArr[boardId].color = color;
     addToLocalStorage();
-    reCreate();
+    
+    allboard = document.querySelectorAll('.board');
+    for(let board of allboard){
+        if(board.getAttribute('board-index-number') == boardId){
+            board.style.backgroundColor = color + 'B3'
+            break
+        }
+    }
+
     exitModal();
 }
 
 let deleteBoard = (boardId) =>{
+    if (boardArr[boardId].cardCounter != 0) {
+        addError('В доске не должно быть задач, чтобы ее удалить!')
+        return    
+    }
+
     boardArr.splice(boardId,1);
     addToLocalStorage();
-    reCreate();
+    allboard = document.querySelectorAll('.board');
+    for(let board of allboard){
+        if(board.getAttribute('board-index-number') == boardId){
+            board.remove();
+            break
+        }
+    }
     exitModal();
 }
 
@@ -227,39 +308,103 @@ let exitModal = () =>{
 
 let addBoardItem = (element) =>{
 
-    let boardItem = document.createElement('div');
+    let boardItem = document.createElement('li');
     boardItem.classList.add('card_body_item');
     boardItem.setAttribute('item-index-number', element.itemId)
 
-    let allboard = document.querySelectorAll('.card');
-    console.log(allboard)
+    let content;
+    
+    content= `<div class="card_header_item">
+                    <div class="card_title_item">${element.title}</div>
+                    <div class="card_tag_item">
+                    <div class="tag_section">`
+ 
+
+    for(let tag of element.tag){
+        content+=`
+        <div class="card_tag" style="background-color: ${colorArr.get(+tag)};"></div> `
+    }
+    
+
+    content += `</div></div></div>
+            <div class="card_date_item">Выполнить до: ${element.expiredDate}</div>`
+
+    boardItem.insertAdjacentHTML('beforeend',content)
+
+    boardItem.addEventListener('click', () => changeBoardItemModal(element.itemId))
+
+    let allboard = document.querySelectorAll('.board');
+    for(let board of allboard){
+        if(board.getAttribute('board-index-number') == element.boardId){
+            board.childNodes[3].childNodes[1].append(boardItem)
+            break
+        }
+    }
 }
 
 let reset = () =>{
     localStorage.removeItem('boardArr');
+    localStorage.removeItem('boardItemArr');
     boardArr = [];
+    boardItemArr = [];
     reCreate();
 }
 
 let reCreate = () =>{
-    cardPlacement.innerHTML = ''
+    boardPlacement.innerHTML = ''
     boardArr.forEach(element => {
         addBoard(element);
     });
+
+    
 }
 
 let boardReCreate = (boardId) =>{
+    allboard = document.querySelectorAll('.board_body_list');
+    allboard.forEach(board => {
+        board.innerHTML = '';
+    });
 
+    boardItemArr.forEach(card => {
+        addBoardItem(card);
+    });
+}
+
+let addError = (error) =>{
+    let body = document.querySelector('body')
+    let errorElement;
+    if (!document.querySelector('.error')) {
+        errorElement = document.createElement('div')
+        errorElement.classList.add('error')
+    }else{
+        errorElement = document.querySelector('.error');
+    }
+    errorElement.innerHTML = error
+    body.append(errorElement)
+    setTimeout(removeError, 3000)
+}
+
+let removeError = () => {
+    if (document.querySelector('.error') != null) {
+        document.querySelector('.error').remove()
+    }
 }
 
 let addToLocalStorage = () =>{
     localStorage.setItem('boardArr', JSON.stringify(boardArr));
+    localStorage.setItem('boardItemArr', JSON.stringify(boardItemArr));
+}
+
+if(localStorage.getItem("boardItemArr") != null){
+    boardItemArr = JSON.parse(localStorage.getItem('boardItemArr'));
+    itemId = boardItemArr[boardItemArr.length-1].itemId+1;
 }
 
 if (localStorage.getItem("boardArr") != null) {
     boardArr = JSON.parse(localStorage.getItem('boardArr'));
     BoardId = boardArr[boardArr.length-1].id+1;
-    //itemId = boardItemArr[boardItemArr-1].itemId+1;
+ 
+   
 }else{
     createBoard(BoardId, "To Do", "#cccccc")
     createBoard(BoardId, "In Progress", "#cccccc")
@@ -267,3 +412,4 @@ if (localStorage.getItem("boardArr") != null) {
 }
 
 reCreate();
+boardReCreate()
